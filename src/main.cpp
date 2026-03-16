0157c0a81734e6cc11b62ca6ece8540083a1f2ac
1withspaghetti <79611123+1withspaghetti@users.noreply.github.com>
2026-03-15 18:16:02 -0700

    more debug info

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,13 @@ void vescTask(void* parameter);
 TaskHandle_t CheckSpeedZero, VescTask, HallEffectTask;
 
 void setup() {
+  #ifdef RGB_BUILTIN
+  neopixelWrite(RGB_BUILTIN, 0, 0, 0);  // Off
+  #endif
+
   Serial.begin(115200);
+  delay(1000); // Wait for serial
+  Serial.println("Initializing...");
 
   #ifdef RGB_BUILTIN
   neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS/2, 0, 0);  // Red
@@ -17,16 +23,21 @@ void setup() {
   // attachInterrupt(digitalPinToInterrupt(2), hallEffectISR, FALLING); //Attaches hall interrupt to pin P2 of Espressif ESP32
   //attachInterrupt(digitalPinToInterrupt(2), hallEffectISR, RISING); //Both seem to work, should test if one is more stable than other on car
   //pinMode(2, INPUT_PULLUP);
+  //Serial.println("Interrupts attached...");
 
   setupBLE();
+  Serial.println("Setup BLE...");
 
   // xTaskCreate(hallEffectTask, "hallEffectTask", 10000, NULL, 1, &HallEffectTask);
   // xTaskCreate(checkSpeedZero, "checkSpeedZero", 10000, NULL, 2, &CheckSpeedZero);
   xTaskCreate(vescTask      , "VescTask"      , 10000, NULL, 2, &VescTask);
+  Serial.println("Setup tasks...");
 
   #ifdef RGB_BUILTIN
   neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS/2, RGB_BRIGHTNESS/2, 0);  // Yellow
   #endif
+
+  Serial.println("Done");
 }
 
 void loop() 
--- src/vesc_link.cpp
+++ src/vesc_link.cpp
@@ -7,55 +7,57 @@ VescUart UART; //object that will recive all the values from vesc
 struct data_packet next_data;
 
 void THD_SERIAL(){
-        if (UART.getVescValues()){
+    Serial.println("Getting values...");
+    if (UART.getVescValues()){
 
-            #ifdef RGB_BUILTIN
-            neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS/2, 0);  // Green
-            #endif
+        #ifdef RGB_BUILTIN
+        neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS/2, 0);  // Green
+        #endif
 
-            //if the data call succeeds, populate the struct.
-            next_data.tempMOSFET = UART.data.tempMosfet;
-            next_data.tempMotor = UART.data.tempMotor;
-            next_data.motorCurrent = UART.data.avgMotorCurrent;
-            next_data.inputCurrent = UART.data.avgInputCurrent;
-            next_data.dutyCycle = UART.data.dutyCycleNow;
-            next_data.tacho = UART.data.tachometerAbs;
-            next_data.rpm = UART.data.rpm;
-            next_data.volts = UART.data.inpVoltage;
-            next_data.wattHours = UART.data.wattHours;
-            next_data.error = UART.data.error;
+        //if the data call succeeds, populate the struct.
+        next_data.tempMOSFET = UART.data.tempMosfet;
+        next_data.tempMotor = UART.data.tempMotor;
+        next_data.motorCurrent = UART.data.avgMotorCurrent;
+        next_data.inputCurrent = UART.data.avgInputCurrent;
+        next_data.dutyCycle = UART.data.dutyCycleNow;
+        next_data.tacho = UART.data.tachometerAbs;
+        next_data.rpm = UART.data.rpm;
+        next_data.volts = UART.data.inpVoltage;
+        next_data.wattHours = UART.data.wattHours;
+        next_data.error = UART.data.error;
 
-            Serial.print("tempMosfet: ");
-            Serial.print(next_data.tempMOSFET);
-            Serial.print(" tempMotor: ");
-            Serial.print(next_data.tempMotor);
-            Serial.print(" motorCurrent: ");
-            Serial.print(next_data.motorCurrent);
-            Serial.print(" inputCurrent: ");
-            Serial.print(next_data.inputCurrent);
-            Serial.print(" dutyCycle: ");
-            Serial.print(next_data.dutyCycle);
-            Serial.print(" tacho: ");
-            Serial.print(next_data.tacho);
-            Serial.print(" rpm: ");
-            Serial.print(next_data.rpm);
-            Serial.print(" volts: ");
-            Serial.print(next_data.volts);
-            Serial.print(" wattHours: ");
-            Serial.print(next_data.wattHours);
-            Serial.print(" error: ");
-            Serial.print(next_data.error);
-            Serial.println();
+        Serial.print("tempMosfet: ");
+        Serial.print(next_data.tempMOSFET);
+        Serial.print(" tempMotor: ");
+        Serial.print(next_data.tempMotor);
+        Serial.print(" motorCurrent: ");
+        Serial.print(next_data.motorCurrent);
+        Serial.print(" inputCurrent: ");
+        Serial.print(next_data.inputCurrent);
+        Serial.print(" dutyCycle: ");
+        Serial.print(next_data.dutyCycle);
+        Serial.print(" tacho: ");
+        Serial.print(next_data.tacho);
+        Serial.print(" rpm: ");
+        Serial.print(next_data.rpm);
+        Serial.print(" volts: ");
+        Serial.print(next_data.volts);
+        Serial.print(" wattHours: ");
+        Serial.print(next_data.wattHours);
+        Serial.print(" error: ");
+        Serial.print(next_data.error);
+        Serial.println();
 
-            sendBLE(next_data);
-        }
+        sendBLE(next_data);
+    }
 }
 
 void UART_setup() {
 
-    Serial1.begin(115200, SERIAL_8N1, 16, 17); 
-    while (!Serial1) {;} //wait for serial to start
+    Serial1.begin(115200, SERIAL_8N1, TX, RX);
     UART.setSerialPort(&Serial1);
+
+    Serial.println("Serial initialized...");
 }
 
 struct data_packet get_packet(){
